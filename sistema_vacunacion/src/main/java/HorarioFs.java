a4753a8841cd21f97bbd7bb08fbbaf0d5e8512f7
wladimiralvarez <lplacencia2024@inf.udec.cl>
2026-06-03 19:54:39 -0400

    Add: esqueleto de clases CentroVacunacion, Cita, EstadoCita, HorarioFs y Vacuna

---
+++ sistema_vacunacion/src/main/java/HorarioFs.java
@@ -0,0 +1,8 @@
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class HorarioFs {
+    private DayOfWeek diaSemana;
+    private LocalTime horaInicio;
+    private LocalTime horaFin;
+}
