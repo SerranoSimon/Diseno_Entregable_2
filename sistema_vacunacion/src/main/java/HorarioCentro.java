5d1e7903489998cfaed78539fd3b03385884a7ac
SerranoSimon <sserrano2024@inf.udec.cl>
2026-06-03 20:54:10 -0400

    implementacion metodo centro.estaAbierto

--- sistema_vacunacion/src/main/java/HorarioCentro.java
+++ sistema_vacunacion/src/main/java/HorarioCentro.java
@@ -1,8 +1,25 @@
 import java.time.DayOfWeek;
 import java.time.LocalTime;
-
 public class HorarioCentro {
     private DayOfWeek diaSemana;
     private LocalTime horaApertura;
     private LocalTime horaCierre;
+
+    public DayOfWeek getDiaSemana() {
+        return diaSemana;
+    }
+
+    public LocalTime getHoraApertura() {
+        return horaApertura;
+    }
+
+    public LocalTime getHoraCierre() {
+        return horaCierre;
+    }
+
+    public HorarioCentro(DayOfWeek diaSemana, LocalTime horaApertura, LocalTime horaCierre) {
+        this.diaSemana = diaSemana;
+        this.horaApertura = horaApertura;
+        this.horaCierre = horaCierre;
+    }
 }
