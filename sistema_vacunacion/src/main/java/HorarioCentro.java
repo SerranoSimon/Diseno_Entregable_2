428fb7988b53e8acfc580f625c5b82c2e7fa12de
wladimiralvarez <lplacencia2024@inf.udec.cl>
2026-06-03 20:09:29 -0400

    Add: creacion de clase HorarioCentro

---
+++ sistema_vacunacion/src/main/java/HorarioCentro.java
@@ -0,0 +1,8 @@
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class HorarioCentro {
+    private DayOfWeek diaSemana;
+    private LocalTime horaApertura;
+    private LocalTime horaCierre;
+}
